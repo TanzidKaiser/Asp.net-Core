0c3e7142c16c5105acad20b25a2ba667e6799045
TanzidKaiser <tanzid.888@gmail.com>
2018-02-04 02:19:57 +0600

    2nd

---
+++ CRUD/Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly DatabaseContext db;
+
+        public StudentController(DatabaseContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+
+            return View(db.Students.ToList());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            db.Students.Add(student);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
